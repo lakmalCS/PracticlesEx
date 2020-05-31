186cf2ffa2cf79758a607a2170e8f3026698bc17
lakmalCS <65708633+lakmalCS@users.noreply.github.com>
2020-05-31 13:29:19 +0530

    commit2

--- Task5/src/lk/ac/kln/fct/learn/generics/core/Entry.java
+++ Task5/src/lk/ac/kln/fct/learn/generics/core/Entry.java
@@ -2,11 +2,9 @@ package lk.ac.kln.fct.learn.generics.core;
 
 import java.util.Vector;
 
-//TODO use generics for the value.
-//You have to start changing the code from here.
+@SuppressWarnings("hiding")
 public class Entry<K,Vector> {
 	private K key;
-	//TODO use generics for the value.
 	private Vector value;	
 	
 	public Entry(K key, Vector value) {
